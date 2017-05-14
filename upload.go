13e275af81037473ed0b83ba25d157565b541d2f
toukii <toukii@toukii.com>
2017-05-14 23:37:04 +0800

    upload

--- upload.go
+++ upload.go
@@ -1,14 +1,24 @@
 package goutils
-
+import (
+	"net/http"
+	"fmt"
+	"encoding/base64"
+	"os"
+	"strings"
+	"bufio"
+	"time"
+)
 
 type UploadHandler struct {}
 
 func (m *UploadHandler)ServeHTTP(w http.ResponseWriter, req *http.Request)  {
 	uri:=req.RequestURI
+	fmt.Println("URI:",uri)
 	if req.Method=="POST" {
-		if "/upload"==uri {
+
+		if strings.HasSuffix(uri,"/upload") {
 			m.Upload(w,req)
-		}else if "/streamUpload"==uri{
+		}else if strings.HasSuffix(uri,"/streamUpload") {
 			m.StreamUpload(w,req)
 		}
 	}
@@ -21,12 +31,12 @@ func (m *UploadHandler) Upload(w http.ResponseWriter, req * http.Request)  {
 	b64data := data[strings.IndexByte(data, ',')+1:]
 	enc:=base64.StdEncoding
 	bs,derr:=enc.DecodeString(b64data)
-	if goutils.CheckErr(derr) {
+	if CheckErr(derr) {
 		fmt.Fprint(w, derr)
 		return
 	}
-	gerr:=goutils.WriteFile("upload.png",bs)
-	if goutils.CheckErr(gerr) {
+	gerr:=WriteFile("MDFs/page.png",bs)
+	if CheckErr(gerr) {
 		fmt.Fprint(w,gerr)
 		return
 	}
@@ -36,9 +46,9 @@ func (m *UploadHandler) Upload(w http.ResponseWriter, req * http.Request)  {
 
 func (m *UploadHandler) StreamUpload(w http.ResponseWriter, req * http.Request)  {
 	enc:=base64.StdEncoding
-	file,err:=os.OpenFile("page.png",os.O_CREATE|os.O_WRONLY,0644)
+	file,err:=os.OpenFile("MDFs/page.png",os.O_CREATE|os.O_WRONLY,0644)
 	defer file.Close()
-	if goutils.CheckErr(err) {
+	if CheckErr(err) {
 		fmt.Fprint(w, err)
 		return
 	}
@@ -54,14 +64,14 @@ func (m *UploadHandler) StreamUpload(w http.ResponseWriter, req * http.Request) 
 		if n1>0 {
 			allbytes = append(allbytes,try[:n1]...)
 		}
-		if goutils.CheckErr(err1) {
+		if CheckErr(err1) {
 			break
 		}
 	}
 
 	dst:=make([]byte,len(allbytes)*2)
 	n3,err:=enc.Decode(dst,allbytes)
-	if !goutils.CheckErr(err) {
+	if !CheckErr(err) {
 		file.Write(dst[:n3])
 	}
 	fmt.Fprint(w,"success")
