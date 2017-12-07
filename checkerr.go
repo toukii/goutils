920b1fafe878baa09714ca1f5428b20689ffc751
shiyongbin <shiyongbin@ezbuy.com>
2017-12-07 21:53:17 +0800

    color-log

--- checkerr.go
+++ checkerr.go
@@ -3,11 +3,25 @@ package goutils
 import (
 	"fmt"
 	"log"
+	"runtime"
+
+	cr "github.com/fatih/color"
+)
+
+var (
+	infoCr = cr.New(cr.BgWhite, cr.FgRed)
+	warnCr = cr.New(cr.BgYellow, cr.FgRed)
+	errCr  = cr.New(cr.BgBlack, cr.FgRed)
 )
 
 func CheckErr(err error) bool {
 	if nil != err {
-		fmt.Println(err)
+		funcName, file, line, ok := runtime.Caller(1)
+		if ok {
+			fmt.Printf("%s Line:%s Func:%s ERR:%s\n", warnCr.Sprint(file), infoCr.Sprint(line), infoCr.Sprint(runtime.FuncForPC(funcName).Name()), errCr.Sprint(err.Error()))
+		} else {
+			errCr.Println(err)
+		}
 		return true
 	}
 	return false
@@ -20,6 +34,7 @@ func CheckNoLogErr(err error) bool {
 	return false
 }
 
+// Deprecated: Use goutils.CheckErr instead.
 func LogCheckErr(err error) bool {
 	if nil != err {
 		log.Println(err)
